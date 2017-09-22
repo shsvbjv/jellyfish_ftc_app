42b6b9335af951c11c6269460c2ff9d6edf5099e
MileKyle <kylepetkovic@gmail.com>
2017-09-21 17:49:36 -0700

    Motor test class addition

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorTest.java
@@ -6,7 +6,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 /**
- * Testing Motors
+ * Testing Motors 00
  */
 
 @TeleOp (name = "Motor Test")
