f186fe8dbfaaef02ae04689f98f8f0dd16fefc9d
oliversun <oliver.sunwc@gmail.com>
2017-09-29 17:54:41 -0700

    Add Manual OpMode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ArcadeTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ArcadeTest.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.Range;
 
 /**
  * Manual with Arcade Drive iooio
@@ -28,20 +29,38 @@ public class ArcadeTest extends LinearOpMode {
         frontLeft.setDirection(DcMotor.Direction.REVERSE);
         backLeft.setDirection(DcMotor.Direction.REVERSE);
 
+        double power;
+        double strafe;
+        double turn;
+        double FL, FR, BL, BR;
+
         waitForStart();
 
         while(opModeIsActive()) {
 
-            double power = -gamepad1.left_stick_y;
-            double strafe = -gamepad1.left_stick_x;
-            double turn = -gamepad1.right_stick_x;
+            if(gamepad1.left_bumper) {
+                power = (-gamepad1.left_stick_y / -gamepad1.left_stick_y) * 0.4;
+                strafe = (-gamepad1.left_stick_x / -gamepad1.left_stick_x) * 0.4;
+                turn = (-gamepad1.right_stick_x / -gamepad1.right_stick_x) * 0.4;
+            } else {
+                power = Range.clip(-gamepad1.left_stick_y, -1, 1);
+                strafe = Range.clip(-gamepad1.left_stick_x, -1, 1);
+                turn = Range.clip(-gamepad1.right_stick_x, -1, 1);
+            }
 
-            frontLeft.setPower(power + turn + strafe);
-            backLeft.setPower(power + turn - strafe);
-            frontRight.setPower(power - turn - strafe);
-            backRight.setPower(power - turn + strafe);
+            FL = power + turn + strafe;
+            BL = power + turn - strafe;
+            FR = power - turn - strafe;
+            BR = power - turn + strafe;
 
+            frontLeft.setPower(FL);
+            backLeft.setPower(BL);
+            frontRight.setPower(FR);
+            backRight.setPower(BR);
 
+            telemetry.addData("Slow", gamepad1.left_bumper);
+            telemetry.addData("Motors", "FL (%.2f), FR (%.2f), BL (%.2f), BR (&.2f)", FL, FR, BL, BR);
+            telemetry.update();
         }
     }
 }
