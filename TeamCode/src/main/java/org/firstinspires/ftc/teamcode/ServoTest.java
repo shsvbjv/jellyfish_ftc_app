03797d84a658ec5ed5aa0b382778fb2ebd01b7f9
oliversun <oliver.sunwc@gmail.com>
2017-10-01 15:37:12 -0700

    servo test OPmode for elevator

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoTest.java
@@ -13,12 +13,14 @@ import com.qualcomm.robotcore.hardware.Servo;
 public class ServoTest extends LinearOpMode {
     private Servo servo1;
     boolean extend;
+    double START_POS = 0.8;
+    double GRAB_POS = 0.3;
 
     @Override
     public void runOpMode() throws InterruptedException {
         servo1 = hardwareMap.servo.get("servo1");
 
-        servo1.setPosition(0.8);
+        servo1.setPosition(START_POS);
         extend = false;
 
         waitForStart();
@@ -26,13 +28,13 @@ public class ServoTest extends LinearOpMode {
         while (opModeIsActive()) {
             if (!extend) {
                 if (gamepad1.a) {
-                    servo1.setPosition(0.8);
+                    servo1.setPosition(START_POS);
                     extend = true;
                     sleep(300);
                 }
             } else {
                 if (gamepad1.a) {
-                    servo1.setPosition(0.3);
+                    servo1.setPosition(GRAB_POS);
                     extend = false;
                     sleep(300);
                 }
