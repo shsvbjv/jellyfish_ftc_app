6a18d9b7a7248bb85d7fb3957e3b27918a25a62c
MileKyle <kylepetkovic@gmail.com>
2017-10-05 17:33:13 -0700

    Merged dev with master

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Winch.java
@@ -0,0 +1,9 @@
+package org.firstinspires.ftc.teamcode;
+//test for the winch
+//motors need to go opposite directions
+
+
+public class Winch {
+
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WinchTest.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode;
+
+/**
+ * Created by KyleP on 10/5/2017.
+ */
+
+public class WinchTest {
+}
