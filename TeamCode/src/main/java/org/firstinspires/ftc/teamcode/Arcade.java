ab292fa02a96a00483191650b3db55bb40f59d8e
oliver-sunwc <oliver.sunwc@gmail.com>
2017-10-08 17:52:47 -0700

    Add files via upload

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Arcade.java
@@ -0,0 +1,119 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.Range;
+
+/**
+ * Manual with Arcade Drive iooio
+ */
+
+@TeleOp(name = "Arcade")
+public class Arcade extends LinearOpMode {
+
+    hMap robot = new hMap();
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        robot.init(hardwareMap);
+
+        double power = 0;
+        double strafe = 0;
+        double turn = 0;
+        double FL, FR, BL, BR;
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+
+            //Winch();
+            power = scaleInput(Range.clip(-gamepad1.right_stick_y, -1, 1));
+            strafe = scaleInput(Range.clip(-gamepad1.right_stick_x, -1, 1));
+            turn = scaleInput(Range.clip(-gamepad1.left_stick_x, -1, 1));
+
+            FL = power - turn + strafe;
+            BL = power - turn - strafe;
+            FR = power + turn - strafe;
+            BR = power + turn + strafe;
+
+            robot.frontLeft.setPower(FL);
+            robot.backLeft.setPower(BL);
+            robot.frontRight.setPower(FR);
+            robot.backRight.setPower(BR);
+
+            telemetry.addData("Motors", "FL (%.2f), FR (%.2f), BL (%.2f), BR (%.2f)", FL, FR, BL, BR);
+            telemetry.addData("Motor Pos", "pFL (%.2f), pFR (%.2f), pBL (%.2f), pBR (%.2f)", robot.frontLeft.getCurrentPosition(), robot.frontRight.getCurrentPosition(), robot.backLeft.getCurrentPosition(), robot.backRight.getCurrentPosition());
+            telemetry.update();
+        }
+    }
+
+    double scaleInput(double dVal) {
+        double[] scaleArray = {0.0, 0.05, 0.09, 0.10, 0.12, 0.15, 0.18, 0.24,
+                0.30, 0.36, 0.43, 0.50, 0.60, 0.72, 0.85, 1.00, 1.00};
+
+        // get the corresponding index for the scaleInput array.
+        int index = (int) (dVal * 16.0);
+        if (index < 0) {
+            index = -index;
+        } else if (index > 16) {
+            index = 16;
+        }
+
+        double dScale = 0.0;
+        if (dVal < 0) {
+            dScale = -scaleArray[index];
+        } else {
+            dScale = scaleArray[index];
+        }
+
+        return dScale;
+    }
+
+    void Winch(){
+        robot.lWinch.setPower(scaleInput(gamepad2.right_stick_y));
+        robot.rWinch.setPower(scaleInput(gamepad2.right_stick_y));
+    }
+    void servo() {
+        if (!robot.bChop) {
+            if (gamepad2.left_bumper) {
+                robot.botServL.setPosition(0);
+                robot.botServR.setPosition(1);
+                robot.bChop = true;
+                sleep(300);
+            }
+        } else {
+            if (gamepad2.left_bumper) {
+                robot.botServL.setPosition(robot.GRAB_CHOP_POS_A);
+                robot.botServR.setPosition(robot.GRAB_CHOP_POS_B);
+                robot.bChop = false;
+                sleep(300);
+            }
+        }
+
+        if (!robot.tChop) {
+            if (gamepad2.right_bumper) {
+                robot.topServL.setPosition(robot.GRAB_CHOP_POS_B);
+                robot.topServR.setPosition(robot.GRAB_CHOP_POS_A);
+                robot.tChop = true;
+                sleep(300);
+            }
+        } else {
+            if (gamepad2.right_bumper) {
+                robot.topServL.setPosition(robot.GRAB_CHOP_POS_B);
+                robot.topServL.setPosition(robot.GRAB_CHOP_POS_A);
+                robot.tChop = false;
+                sleep(300);
+            }
+        }
+        telemetry.addData("BL", robot.botServL.getPosition());
+        telemetry.addData("BR", robot.botServR.getPosition());
+        telemetry.addData("TL", robot.topServL.getPosition());
+        telemetry.addData("TR", robot.topServR.getPosition());
+        telemetry.update();
+    }
+}
+
+
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hMap.java
@@ -0,0 +1,90 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+/**
+ * Created by oliversun on 10/7/17.
+ */
+
+public class hMap {
+    /* Motors */
+
+    //Wheels
+    public DcMotor  frontLeft   ;
+    public DcMotor  frontRight  ;
+    public DcMotor  backLeft    ;
+    public DcMotor  backRight   ;
+
+    //Winch
+    public DcMotor  lWinch      ;
+    public DcMotor  rWinch      ;
+
+    /* Servos */
+
+    //Chopstick Servos
+    public Servo    topServL    ;
+    public Servo    topServR    ;
+    public Servo    botServL    ;
+    public Servo    botServR    ;
+
+    //Sensor Arm Servo
+    //public Servo    armServo    ;
+
+    //Values for the chopsticks and tail
+    public static final double START_TAIL_POS    = 0.8;
+    public static final double GRAB_TAIL_POS     = 0.3;
+    public static final double START_CHOP_POS_A  = 0.0;
+    public static final double START_CHOP_POS_B  = 1.0;
+    public static final double GRAB_CHOP_POS_A   = 0.4;
+    public static final double GRAB_CHOP_POS_B   = 0.6;
+
+    //boolean for servo function
+    public boolean tail;
+    public boolean tChop;
+    public boolean bChop;
+
+    HardwareMap hwMap;
+
+    public hMap() {}
+
+    public void init(HardwareMap ahwMap) {
+        hwMap = ahwMap;
+
+        /* Motors */
+
+        //Wheels
+        frontLeft   = hwMap.get(DcMotor.class, "frontLeft")     ;
+        frontRight  = hwMap.get(DcMotor.class, "frontRight")    ;
+        backLeft    = hwMap.get(DcMotor.class, "backLeft")      ;
+        backRight   = hwMap.get(DcMotor.class, "backRight")     ;
+
+        frontLeft.setDirection(DcMotor.Direction.REVERSE)       ;
+        backLeft.setDirection(DcMotor.Direction.REVERSE)        ;
+
+        frontLeft.setPower(0);
+        frontRight.setPower(0);
+        backLeft.setPower(0);
+        backRight.setPower(0);
+
+        //Winch
+        lWinch      = hwMap.get(DcMotor.class, "lWinch")        ;
+        rWinch      = hwMap.get(DcMotor.class, "rWinch")        ;
+
+        lWinch.setPower(0);
+        rWinch.setPower(0);
+
+        /* Servos */
+        topServL    = hwMap.get(Servo.class, "topServL")        ;
+        topServR    = hwMap.get(Servo.class, "topServR")        ;
+        botServL    = hwMap.get(Servo.class, "topServL")        ;
+        topServR    = hwMap.get(Servo.class, "botServR")        ;
+        //armServo    = hwMap.get(Servo.class, "armServo")        ;
+
+        botServL.setPosition(START_CHOP_POS_A);
+        botServR.setPosition(START_CHOP_POS_B);
+        topServL.setPosition(START_CHOP_POS_B);
+        topServR.setPosition(START_CHOP_POS_A);
+    }
+}
