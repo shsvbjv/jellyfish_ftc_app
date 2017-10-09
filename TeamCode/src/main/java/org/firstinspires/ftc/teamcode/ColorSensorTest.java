0e002cefd3e665bed5ffc264eaf13c998ca8b690
FerannoDad <feranno.liu@gmail.com>
2017-10-08 18:03:42 -0700

    color sensor fix so that it shows the average

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorSensorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorSensorTest.java
@@ -28,7 +28,39 @@ public class ColorSensorTest extends LinearOpMode {
         telemetry.addData("red value", color_sensor.red());
 
 
-        if(color_sensor.red()>color_sensor.blue()+5){
+        if(color_sensor.red()>color_sensor.blue()){
+            telemetry.addData("The color is ", "red");
+            return true;
+        }
+        else{
+            telemetry.addData("The color is ", "blue");
+            return false;
+        }
+    }
+
+    public boolean isJewelRedFinal(){
+        int count=0;
+        double bar=0.75;
+        int redcount=0;
+        double redpercent;
+        int bluecount=0;
+        double bluepercent=0;
+
+        while(bar<0.75 && count<20){
+            count+=1;
+            if(isJewelRed()==true){
+                redcount+=1;
+                redpercent=redcount/count;
+                bar=redpercent;
+            }
+            else{
+                bluecount+=1;
+                bluepercent=bluecount/count;
+                bar=bluepercent;
+            }
+
+        }
+        if(redcount>bluecount){
             return true;
         }
         else{
